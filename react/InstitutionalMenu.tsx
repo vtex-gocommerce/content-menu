75c60772f7953c82ec871a652b4fb665474642df
Augusto Lazaro <aemlazaro@gmail.com>
2019-07-23 17:50:24 -0300

    Make query to get custom routes

--- react/InstitutionalMenu.tsx
+++ react/InstitutionalMenu.tsx
@@ -1,11 +1,57 @@
 import * as React from 'react'
+import { Query } from 'react-apollo'
+import { defineMessages } from 'react-intl'
 
-const Menu = () => {
-  return (
-    <div>
-      <span>Insitutional Menu</span>
-    </div>
-  )
+import { Link } from 'vtex.render-runtime'
+
+import ROUTES_QUERY from './graphql/routes.graphql'
+
+interface Route {
+  context: string | null
+  path: string
+  title: string
+  routeId: string
 }
 
-export default Menu
+const messages = defineMessages({
+  menuTitle: {
+    defaultMessage: '',
+    id: 'admin/editor.future-menu.title',
+  },
+})
+
+const InstitutionalMenu = () => (
+  <Query query={ROUTES_QUERY} variables={{ domain: 'store' }}>
+    {({ data, loading, error }: any) => {
+      if (error || loading) {
+        // TODO add loader and error message
+        return null
+      }
+
+      const pagesRoutes = (data.routes || [])
+        .filter((route: Route) => (route.context || '').endsWith('ContentPageContext'))
+
+      return (
+        <nav>
+          {pagesRoutes.map((route: Route) => (
+            <div className=" mh6 pv2">
+              <Link
+                className={'no-underline pointer t-body c-on-base pointer'}
+                key={route.routeId}
+                to={route.path}
+              >
+                {route.title}
+              </Link>
+            </div>
+          ))}
+        </nav>
+      )        
+    }}
+  </Query>
+)
+
+InstitutionalMenu.getSchema = () => ({
+  title: messages.menuTitle.id,
+})
+
+export default InstitutionalMenu
