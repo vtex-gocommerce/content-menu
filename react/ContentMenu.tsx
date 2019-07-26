2ce48958540e0c93ff723a8a6bc4afc4b07c31cf
Augusto Lazaro <aemlazaro@gmail.com>
2019-07-26 11:49:09 -0300

    Apply content schema

--- react/ContentMenu.tsx
+++ react/ContentMenu.tsx
@@ -3,13 +3,18 @@ import { Query } from 'react-apollo'
 import { defineMessages } from 'react-intl'
 
 import ROUTES_QUERY from './graphql/routes.graphql'
-import { Menu, MenuItem, MenuSchema } from 'vtex.menu'
+import { Menu, MenuItem } from 'vtex.menu'
 
 interface Route {
   context: string | null
   path: string
   title: string
   routeId: string
+}
+
+interface MenuSchema {
+  visible: boolean
+  label: string | null
 }
 
 const messages = defineMessages({
@@ -19,41 +24,58 @@ const messages = defineMessages({
   },
 })
 
-const ContentMenu: StorefrontFunctionComponent<MenuSchema> = props => (
-  <Query query={ROUTES_QUERY} variables={{ domain: 'store' }}>
-    {({ data, loading, error }: any) => {
-      if (error || loading) {
-        // TODO add loader and error message
-        return null
-      }
+const ContentMenu: StorefrontFunctionComponent<MenuSchema> = ({ visible, label }) => {
+  if (!visible) { return null }
 
-      const pagesRoutes = (data.routes || [])
-        .filter((route: Route) => (route.context || '').endsWith('ContentPageContext'))
+  return (
+    <Query query={ROUTES_QUERY} variables={{ domain: 'store' }}>
+      {({ data, loading, error }: any) => {
+        if (error || loading) {
+          // TODO add loader and error message
+          return null
+        }
 
-      return (
-        <Menu
-          {...props}
-          orientation={'vertical'}
-        >
-          {pagesRoutes.map(({ title, path, routeId }: Route) => (
-            <MenuItem
-              id={routeId}
-              type={'custom'}
-              highlight={false}
-              itemProps={{
-                type: 'internal',
-                text: title,
-                noFollow: false,
-                tagTitle: title,
-                href: path,
-              }}
-            />
-          ))}
-        </Menu>
-      )
-    }}
-  </Query>
-)
+        const pagesRoutes = (data.routes || [])
+          .filter((route: Route) => (route.context || '').endsWith('ContentPageContext'))
+
+        return (
+          <Menu
+            {...(label && {
+              title: {
+                id: '',
+                type: 'custom',
+                highlight: false,
+                itemProps: {
+                  text: label,
+                  type: 'internal',
+                  noFollow: true,
+                  tagTitle: label,
+                  href: '',
+                }
+              }
+            })}
+            orientation={'vertical'}
+          >
+            {pagesRoutes.map(({ title, path, routeId }: Route) => (
+              <MenuItem
+                id={routeId}
+                type={'custom'}
+                highlight={false}
+                itemProps={{
+                  type: 'internal',
+                  text: title,
+                  noFollow: false,
+                  tagTitle: title,
+                  href: path,
+                }}
+              />
+            ))}
+          </Menu>
+        )
+      }}
+    </Query>
+  )
+}
 
 ContentMenu.getSchema = () => ({
   title: messages.menuTitle.id,
